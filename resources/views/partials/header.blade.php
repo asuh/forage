a4258c3d537e62ba0de4eac2d3b98b67c37dd6da
Micah <micah@asuh.com>
2024-06-01 09:04:07 -0400

    Simplify navigation classes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,7 +8,7 @@
   @if (has_nav_menu('primary_navigation'))
     <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
       <button class="toggle-button" type="button" aria-expanded="false" aria-controls="nav-primary">Menu</button>
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-primary-wrapper js-nav-mobile', 'menu_id' => 'nav-primary', 'container' => false]) !!}
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav-list', 'menu_id' => 'nav-primary', 'container' => false]) !!}
     </nav>
   @endif
 </header>
