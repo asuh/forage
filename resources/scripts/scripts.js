fa11db9f76ddff881f34ddb2b047c9a466425d7b
Micah <micah@asuh.com>
2024-06-04 13:10:11 -0400

    Add DOMContentLoaded as example

--- resources/scripts/scripts.js
+++ resources/scripts/scripts.js
@@ -7,3 +7,15 @@ class Test {
 const test = new Test();
 
 test.log();
+
+
+/**
+ * Load Events
+ * 
+ * Wait for the page to load the content,
+ * then apply any scripts contained in this eventlistener
+ * 
+ **/ 
+document.addEventListener('DOMContentLoaded', () => {
+  // Load your scripts inside here
+});
