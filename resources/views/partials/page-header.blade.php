b53ff4f24a76a946c36f0339c3115dfa3ad3eae3
Micah <micah@asuh.com>
2024-06-14 17:00:20 -0400

    Hide page header container if homepage

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,5 +1,5 @@
-<div class="page-header">
-  @if (!is_front_page() && !is_home())
-    <h1 class="page-title p-name">{!! get_the_title() !!}</h1>
-  @endif
-</div>
+@if (!is_front_page() && !is_home())
+    <div class="page-header">
+        <h1 class="page-title p-name">{!! get_the_title() !!}</h1>
+    </div>
+@endif
