6711a72a42f830a16260334ccfefe23a4b503477
Micah <micah@asuh.com>
2024-06-18 16:53:25 -0400

    Add comments for .p-note of h-card

--- resources/views/partials/h-card.blade.php
+++ resources/views/partials/h-card.blade.php
@@ -11,5 +11,9 @@
             <span class="p-given-name">{{ get_the_author_meta('first_name') }}</span>
             <span class="screen-reader-text p-family-name">{{ get_the_author_meta('last_name') }}</span>
         </span>
+        {{-- Many h-cards contain a notes section where you can add a biography or short description of the author. Feel free to uncomment the line below and add anything there, then remove this line.
+
+        You can validate this h-card setup at https://indiewebify.me/validate-h-card/ --}}
+        {{-- <span class="p-note"></span> --}}
     </a>
 </div>
