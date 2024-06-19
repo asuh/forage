2537d42ba05aab2725ed0d64b3c17f3e3608aab8
Micah <micah@asuh.com>
2024-06-18 23:55:51 -0400

    p-note not active by default

--- resources/views/partials/h-card.blade.php
+++ resources/views/partials/h-card.blade.php
@@ -20,9 +20,9 @@
             <span class="p-given-name">{{ get_the_author_meta('first_name', $id) }}</span>
             <span class="screen-reader-text p-family-name">{{ get_the_author_meta('last_name', $id) }}</span>
         </span>
-        {{-- Many h-cards contain a notes section where you can add a biography or short description of the author. Feel free to uncomment the line below and add a bio to the profile, then delete this line.
+        {{-- Many h-cards contain a notes section where you can add a biography or short description of the author. Feel free to uncomment the line below, add a bio to the default profile (found in the Users profile section of WordPress), and then delete this line as well.
 
         You can validate this h-card setup at https://indiewebify.me/validate-h-card/ --}}
-        <div class="p-note">{{ the_author_meta('description', $id) }}</div>
+        {{-- <div class="p-note">{{ the_author_meta('description', $id) }}</div> --}}
     </a>
 </div>
