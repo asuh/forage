455bcf1bff30ab21198a9fc9ffb675b66d84c680
Micah <micah@asuh.com>
2024-06-20 17:32:31 -0400

    Toggle Navigation as toggle button text
    It makes sense to allow CSS to apply text states with `content()` rule and JS to support accessibility

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,7 +8,7 @@
 
     @if (has_nav_menu('primary_navigation'))
         <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-            <button class="toggle-button" type="button" aria-expanded="false" aria-controls="nav-primary">Menu</button>
+            <button class="toggle-button" type="button" aria-expanded="false" aria-controls="nav-primary"><span class="visuallyhidden">Toggle Navigation</span></button>
             {!! wp_nav_menu([
                 'theme_location' => 'primary_navigation',
                 'menu_class' => 'nav-list',
