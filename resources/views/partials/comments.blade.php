c1b8724cdb1c38bc0d3ca3660f2ed7e068eeaf87
Micah <micah@asuh.com>
2024-07-01 09:59:23 -0400

    Update Comments template with corrected conditionals
    Comments weren't showing right because conditionals were too aggressive

--- resources/views/partials/comments.blade.php
+++ resources/views/partials/comments.blade.php
@@ -4,19 +4,20 @@ if (post_password_required()) {
 }
 ?>
 
+<? if (get_comments_number() != '0') { ?>
+<h2 class="comments-title">
+    Responses
+</h2>
+
 <section id="comments" class="comments">
-  <? if (have_comments()) { ?>
     <? if (comments_open()) { ?>
     <div class="button">
-      <a href="#respond">Write a new comment below</a>
+        <a href="#respond">Write a new comment below</a>
     </div>
     <? } ?>
-    <h2 class="comments-title">
-      Responses
-    </h2>
 
     <ol class="comment-list">
-      <?
+        <?
         wp_list_comments(
           [
             'style' => 'ol',
@@ -29,24 +30,18 @@ if (post_password_required()) {
     </ol><!-- .comment-list -->
 
     <? if (get_comment_pages_count() > 1 && get_option('page_comments')) { ?>
-      <nav aria-label="Comment">
+    <nav aria-label="Comment">
         <ul class="pager">
-          <? if (get_previous_comments_link()) { ?>
+            <? if (get_previous_comments_link()) { ?>
             <li class="previous"><? previous_comments_link(__('&larr; Older comments')) ?></li>
-          <? } ?>
-          <? if (get_next_comments_link()) { ?>
+            <? } ?>
+            <? if (get_next_comments_link()) { ?>
             <li class="next"><? next_comments_link(__('Newer comments &rarr;')) ?></li>
-          <? } ?>
+            <? } ?>
         </ul>
-      </nav>
+    </nav>
     <? } ?>
-  <? } ?>
 
-  <? if (!comments_open() && get_comments_number() != '0' && post_type_supports(get_post_type(), 'comments')) { ?>
-    <div class="alert alert-warning">
-      Comments are closed.
-    </div>
-  <? } ?>
-
-  <? comment_form() ?>
+    <? comment_form() ?>
 </section>
+<? } ?>
