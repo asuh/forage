4f02d8614395ef0f349630577eacd1a76f0ed37d
Micah <micah@asuh.com>
2024-11-17 13:15:41 -0500

    Remove Stylelint order dependencies
    1. This is a subjective preference, better left to the author to choose if they want specific ordering rules
    2. Removing this as a kind of preparation for using Biome for linting styling

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,20 +1,6 @@
-import { propertyGroups } from 'stylelint-config-clean-order';
-
 export default {
-  extends: ['stylelint-config-clean-order'],
   rules: {
     'no-empty-source': null,
     'selector-class-pattern': null,
-    'order/properties-order': [
-      propertyGroups.map((properties) => ({
-        noEmptyLineBetween: true,
-        emptyLineBefore: 'never',
-        properties,
-      })),
-      {
-        severity: 'warning',
-        unspecified: 'bottomAlphabetical',
-      },
-    ],
   },
 };
