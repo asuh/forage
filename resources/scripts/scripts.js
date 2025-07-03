082ef401a9a711aebf005b75786f205716030eae
Micah <micah@asuh.com>
2025-07-03 16:23:25 -0400

    Update packages, comment out scripts code to prevent issues

--- resources/scripts/scripts.js
+++ resources/scripts/scripts.js
@@ -1,10 +1,10 @@
 /**
  * Load Events
- * 
+ *
  * Wait for the page to load the content,
  * then apply any scripts contained in this eventlistener
- * 
- **/ 
-document.addEventListener('DOMContentLoaded', () => {
-  // Load your scripts inside here
-});
+ *
+ **/
+// document.addEventListener('DOMContentLoaded', () => {
+//   // Load your scripts inside here
+// });
