70909098f2ba0f8d631e3361d2e89d7adb75d341
Micah <micah@asuh.com>
2025-11-08 20:41:44 -0500

    Revert deregistering i18n
    It also seems to deregister or deenqueue the scripts assets

--- app/Setup.php
+++ app/Setup.php
@@ -287,7 +287,6 @@ class Setup
         if (! $hasForm) {
             add_filter('wpcf7_load_js', '__return_false');
             add_filter('wpcf7_load_css', '__return_false');
-            wp_deregister_script( 'wp-i18n' );
 
             remove_action(
                 'wp_enqueue_scripts',
