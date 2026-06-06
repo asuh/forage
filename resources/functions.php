13370830ff220a654b500c527d439619874bacbf
Micah <micah@asuh.com>
2026-06-06 13:59:51 -0400

    HMR host follows HTTP_HOST automatically, expose dev server to network

--- resources/functions.php
+++ resources/functions.php
@@ -4,7 +4,12 @@ define('FORAGE_VERSION', '1.0.0');
 define('FORAGE_ROOT', str_replace(ABSPATH, '/', dirname(__DIR__, 1)));
 define('FORAGE_PATH', dirname(__DIR__, 1));
 define('FORAGE_URI', site_url(FORAGE_ROOT));
-define('FORAGE_HMR_HOST', 'http://localhost:5173');
+
+if (! defined('FORAGE_HMR_HOST')) {
+    $hmrHost = strtok($_SERVER['HTTP_HOST'] ?? 'localhost', ':');
+    define('FORAGE_HMR_HOST', "http://{$hmrHost}:5173");
+}
+
 define('FORAGE_HMR_URI', FORAGE_HMR_HOST . FORAGE_ROOT);
 define('FORAGE_DIST_PATH', FORAGE_PATH . '/dist');
 define('FORAGE_DIST_URI', FORAGE_URI . '/dist');
--- vite.config.js
+++ vite.config.js
@@ -54,6 +54,7 @@ export default defineConfig({
 
   server: {
     cors: true,
+    host: true,
   },
 
   resolve: {
