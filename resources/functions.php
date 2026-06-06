d5ff568ad715a74e4cf9bbee999534e48991eda3
Micah <micah@asuh.com>
2026-06-05 23:57:39 -0400

    bump version to 1.0.0

--- resources/functions.php
+++ resources/functions.php
@@ -1,6 +1,6 @@
 <?php // phpcs:ignore PSR1.Files.SideEffects.FoundWithSymbols
 
-define('FORAGE_VERSION', '0.1.4');
+define('FORAGE_VERSION', '1.0.0');
 define('FORAGE_ROOT', str_replace(ABSPATH, '/', dirname(__DIR__, 1)));
 define('FORAGE_PATH', dirname(__DIR__, 1));
 define('FORAGE_URI', site_url(FORAGE_ROOT));
